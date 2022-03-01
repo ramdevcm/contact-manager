f8265d0f102d13b079a3c4a2a8f2e6fc567cc6a6
ramdevcm <ramdevcm@gmail.com>
2022-03-01 12:14:04 +0530

    url

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ class App extends Component {
     return (
       <Container className="spacing">
         <div className="ui two item menu">
-          <NavLink className="item" activeClassName="active" exact to="/lists">
+          <NavLink className="item" activeClassName="active" exact to="/">
             Contacts List
           </NavLink>
           <NavLink className="item" activeClassName="active" exact to="/contacts/new">
             Add Contact
           </NavLink>
         </div>
-        <Route exact path="/lists" component={ContactListPage}/>
+        <Route exact path="/" component={ContactListPage}/>
         <Route path="/contacts/new" component={ContactFormPage}/>
         <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
       </Container>
