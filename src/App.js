fbbce4e37881ca26baf7dacef20156335c1e6d10
ramdevcm <68838234+ramdevcm@users.noreply.github.com>
2022-03-17 15:25:26 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { NavLink, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import ContactListPage from './pages/contact-list-page';
 import ContactFormPage from './pages/contact-form-page';
-import LoginFormPage from './pages/LoginFormPage';
-import RegisterFormPage from './pages/RegisterFormPage';
+// import LoginFormPage from './pages/LoginFormPage';
+// import RegisterFormPage from './pages/RegisterFormPage';
 class App extends Component {
   render() {
     return (
@@ -17,20 +17,20 @@ class App extends Component {
           <NavLink className="item" activeClassName="active" exact to="/contacts/new">
             Add Contact
           </NavLink>
-          <NavLink className="item" activeClassName="active" exact to="/login">
-            Login
-          </NavLink>
-          <NavLink className="item" activeClassName="active" exact to="/register">
-            Register
-          </NavLink>
+//           <NavLink className="item" activeClassName="active" exact to="/login">
+//             Login
+//           </NavLink>
+//           <NavLink className="item" activeClassName="active" exact to="/register">
+//             Register
+//           </NavLink>
 
         </div>
         <Route exact path="/" component={ContactListPage}/>
         <Route exact path="/lists" component={ContactListPage}/>
         <Route path="/contacts/new" component={ContactFormPage}/>
         <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
-        <Route path="/login" component={LoginFormPage}/>
-        <Route path="/register" component={RegisterFormPage}/>
+//         <Route path="/login" component={LoginFormPage}/>
+//         <Route path="/register" component={RegisterFormPage}/>
       </Container>
     );
   }
